36aa711a19896c8b0f0211e4499448fab9590414
davitti <abuadonald@gmail.com>
2025-02-13 05:18:35 +0100

    Refactor response handling in getTasksController.

--- src/routes/tasks/tasks.controllers.ts
+++ src/routes/tasks/tasks.controllers.ts
@@ -3,9 +3,9 @@ import { Request, Response } from "express";
 export async function getTasksController(_: Request, res: Response) {
 	try {
 		// Async task here
-		return res.status(200).json({ success: true });
+		res.status(200).json({ success: true });
 	} catch (error) {
 		console.log("---getTasksController---\n", error);
-		return res.status(400).json({ error });
+		res.status(400).json({ error });
 	}
 }
